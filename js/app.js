177ed93f6c79e64f81845b25747425d630e57a19
hridoy281810 <hridoy01724281810@gmail.com>
2023-03-03 15:19:29 +0600

    show all btn show dynamic data add

--- js/app.js
+++ js/app.js
@@ -8,18 +8,17 @@ const aiCategorisFetch = () => {
 }
 
 const showAiCategoris = data => {
-    // console.log(data)
+    console.log(data.tools.length)
     const AiCategorisContainer = document.getElementById('ai-container');
-    // items = items.slice(0, 6);
-//   display 6 item only 
-// const showAll = document.getElementById('ahow-all');
-// if(data.length > 6){
-//       data = data.slice(0, 6);
-//     showAll.classList.remove('d-none')
-//   }
-//   else{
-//     showAll.classList.add('d-none');
-// }
+    // display 6 item only 
+    const showAll = document.getElementById('ahow-all');
+    if(data.tools.length > 6){
+      data.tools = data.tools.slice(0, 6);
+    showAll.classList.remove('d-none')
+  }
+  else{
+    showAll.classList.add('d-none');
+}
 
 data.tools.forEach(singleAiCategories => {
     const aiDiv = document.createElement('div');
@@ -115,4 +114,49 @@ const displayAiDetails = data => {
   </div>
     `
    } 
+
+   const aiCategorisBtn = () => {
+    toggleSpinner(true) 
+    const url = `https://openapi.programming-hero.com/api/ai/tools`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => showAiCategoris2(data.data))
+}
+
+const showAiCategoris2 = data => {
+  console.log(data.tools.length)
+  const AiCategorisContainer = document.getElementById('ai-container');
+  AiCategorisContainer.innerHTML = '';
+data.tools.forEach(singleAiCategories => {
+  const aiDiv = document.createElement('div');
+  aiDiv.classList.add('col');
+  aiDiv.innerHTML = `
+   <div class="card h-100">
+                  <img src="${singleAiCategories.image ? singleAiCategories.image: 'No Img Found'}" class="card-img-top p-2 " alt="..." style="width: 355px; height: 200px;">
+                  <div class="card-body">
+                    <h5 class="card-title">Features</h5>
+                       <ol>
+                          <li>${singleAiCategories.features[0] ? singleAiCategories.features[0]: 'No data Found'}</li>
+                          <li>${singleAiCategories.features[1] ? singleAiCategories.features[1]: 'No data Found'}</li>
+                          <li>${singleAiCategories.features[2] ? singleAiCategories.features[2]: 'No data Found'}</li> 
+                       </ol> 
+                       <hr>
+                      <div class="d-flex justify-content-between ">
+                          <div> <h5 class="card-title">${singleAiCategories.name ? singleAiCategories.name: 'No data Found'}</h5>
+                              <i class="fa-solid fa-calendar-week text-secondary"></i> <span class="text-secondary">${singleAiCategories.published_in ? singleAiCategories.published_in: 'No data Found'}</span></div>
+                          <div class="d-flex justify-content-center">
+                            <button  onclick="aiDetailsById('${singleAiCategories.id}')" class="btn bg-warning-subtle p-3" style="clip-path: circle(40%); "  data-bs-toggle="modal" data-bs-target="#aiDetailModal"><i class="fa-solid fa-arrow-right"></i></button>
+                          </div>
+                      </div>
+                  </div>
+                </div>
+  `
+  AiCategorisContainer.appendChild(aiDiv);
+  toggleSpinner(false) 
+  })
+}
+   document.getElementById('btn-showall').addEventListener('click', function(){
+    
+    aiCategorisBtn()
+   })
 aiCategorisFetch()
